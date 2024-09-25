c19336a3d86cdcb89cc52ce60c23cb7348b2e6eb
charmaine <117857989+charmieboo@users.noreply.github.com>
2024-09-25 16:24:27 +0800

    push

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+  function filterProjects(category) {
+    let projects = document.querySelectorAll('.project-card');
+    projects.forEach((project) => {
+      if (category === 'all' || project.dataset.category === category) {
+        project.style.display = 'block';
+      } else {
+        project.style.display = 'none';
+      }
+    });
+  
+    let buttons = document.querySelectorAll('.btn-filter');
+    buttons.forEach((button) => {
+      if (button.textContent.toLowerCase() === category) {
+        button.classList.add('active');
+      } else {
+        button.classList.remove('active');
+      }
+    });
+  }
+  
